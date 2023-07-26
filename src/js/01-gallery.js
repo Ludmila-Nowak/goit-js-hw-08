557bbc553ebeaf8e32b93391a857e9b80faea176
Ludmila-Nowak <126966727+Ludmila-Nowak@users.noreply.github.com>
2023-07-26 18:02:33 +0200

    task  1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,30 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
-// Change code below this line
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const galleryListEl = document.querySelector('.gallery');
+
+const createImgMarkup = array => {
+  const markup = array
+    .map(el => {
+      return `<li class="gallery__item">
+<a class="gallery__link"
+  href="${el.original}">
+  <img class="gallery__image"
+    src="${el.preview}"
+    alt="${el.description}"/></a></li>`;
+    })
+    .join('');
+
+  galleryListEl.innerHTML = markup;
+};
+
+createImgMarkup(galleryItems);
+
+const lightbox = new SimpleLightbox('.gallery__link', {
+  captionDelay: 250,
+  captionsData: 'alt',
+});
 
 console.log(galleryItems);
